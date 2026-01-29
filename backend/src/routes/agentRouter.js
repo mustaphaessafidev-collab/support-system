23ec8920b5ed098e39e3e3be157c63852e2faf29
Mustapha Essafi <mustapha.essafi.dev@gmail.com>
2026-01-29 12:32:31 +0100

    work in part admin middleware only admin he can add agent

--- backend/src/routes/agentRouter.js
+++ backend/src/routes/agentRouter.js
@@ -1,11 +1,12 @@
 const express =require('express')
 const  AddAgentgs  = require('../controllers/adminAddAgantsController')
 const protect = require('../middlewares/authMiddleware');
+const isAdmin = require('../middlewares/adminMiddleware');
 
 
 
 const router=express.Router();
 
-router.post('/addAgent',protect,AddAgentgs)
+router.post('/addAgent',protect,isAdmin,AddAgentgs)
 
 module.exports=router;
